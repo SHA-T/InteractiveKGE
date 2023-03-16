ab4dc54e098801c6ee29958b6ccda170d128bbfa
SHA-T <tel.shamoyan@gmail.com>
2023-03-16 03:20:48 +0100

    Updated utility scripts

--- util/create_entities_dict.py
+++ util/create_entities_dict.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
 
+"""
+This script creates an entities.dict file from the train, valid and test set under the specified data sub-folder.
+"""
+
+DATASET_NAME = "Yamanishi_with_Similarity_Information"
+
 if __name__ == "__main__":
-    df_train = pd.read_csv("./data/Yamanishi_with_Similarity_Information/train.txt", sep="\t",
+    df_train = pd.read_csv(f"../data/{DATASET_NAME}/train.txt", sep="\t",
                            names=['head', 'relation', 'tail'])
-    df_valid = pd.read_csv("./data/Yamanishi_with_Similarity_Information/valid.txt", sep="\t",
+    df_valid = pd.read_csv(f"../data/{DATASET_NAME}/valid.txt", sep="\t",
                            names=['head', 'relation', 'tail'])
-    df_test = pd.read_csv("./data/Yamanishi_with_Similarity_Information/test.txt", sep="\t",
+    df_test = pd.read_csv(f"../data/{DATASET_NAME}/test.txt", sep="\t",
                            names=['head', 'relation', 'tail'])
 
     print(df_train)
@@ -14,9 +20,8 @@ if __name__ == "__main__":
     df_all = pd.concat([df_train, df_valid, df_test])
     df_entities = pd.concat([df_all['head'].drop_duplicates(), df_all['tail'].drop_duplicates()],
                             ignore_index=True).drop_duplicates()
-    print(df_entities)
 
     df_entities = df_entities.reset_index(drop=True)
 
-    df_entities.to_csv('./data/Yamanishi_with_Similarity_Information/entities.dict', sep="\t", index=True,
+    df_entities.to_csv(f"../data/{DATASET_NAME}/entities.dict", sep="\t", index=True,
                        header=False)
