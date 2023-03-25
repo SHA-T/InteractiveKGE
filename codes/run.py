51774563aa3f90d100fa4bc3d5f9c7d2fb1845e6
SHA-T <tel.shamoyan@gmail.com>
2023-03-25 20:00:38 +0100

    Fixed bug with freezing pretrained embeddings

--- codes/run.py
+++ codes/run.py
@@ -350,7 +350,7 @@ def main(args):
 
         # Freeze embeddings of all pretrained relations
         # For now the assumption is that all relations except the first are from the mock dataset 'pretrain.txt'
-        kge_model.relation_embedding[1:].detach_()
+        kge_model.relation_embedding[1:].detach()
 
     #################### Test
     print("\nrelation embeddings after pretraining:\n", kge_model.relation_embedding, "\n")
